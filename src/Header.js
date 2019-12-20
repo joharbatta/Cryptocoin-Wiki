d77d57249ea039aac2bb642c3a1e8e4e2217cea1
joharbatta <joharbatta@gmail.com>
2019-12-21 01:55:08 +0530

    Header Added

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = function() {
+    return (
+        <div className="header">
+        <div>Crypto Wiki</div>
+        </div>
+    )
+}
+
+export default Header;
