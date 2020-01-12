518e3f4aa565d5586c22f64fc697c98ec76a71de
joharbatta <joharbatta@gmail.com>
2020-01-12 23:19:28 +0530

    Updates

---
+++ precache-manifest.c9305c284a775bdd5f771e168fdc7ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff749fd5e8e18dc07f4f2db8f65b91dc",
+    "url": "/joharbatta/Cryptocoin-Wiki/index.html"
+  },
+  {
+    "revision": "cc691714d7a6a3455554",
+    "url": "/joharbatta/Cryptocoin-Wiki/static/css/main.f94cadac.chunk.css"
+  },
+  {
+    "revision": "0fca43a85283ceb71c05",
+    "url": "/joharbatta/Cryptocoin-Wiki/static/js/2.0044e58d.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/joharbatta/Cryptocoin-Wiki/static/js/2.0044e58d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc691714d7a6a3455554",
+    "url": "/joharbatta/Cryptocoin-Wiki/static/js/main.1255c4f1.chunk.js"
+  },
+  {
+    "revision": "ef397287aa398c97a3b4",
+    "url": "/joharbatta/Cryptocoin-Wiki/static/js/runtime-main.85d75a9e.js"
+  }
+]);
